e7623800308a0192b721c2bd349facdad595508e
karthik-vectrous <karthikn@vectrous.com>
2025-09-02 19:16:58 +0530

    added mapping of first list to second

---
+++ mapAtoB.py
@@ -0,0 +1,16 @@
+ids=[101,102,101]
+names=["A","B","C"]
+freq={}
+i=0
+j=0
+while i<len(ids) and j<len(names):
+    if ids[i] not in freq:
+        freq[ids[i]]=names[j]
+
+    else:
+        freq[ids[i]]+=", "+names[j]
+
+    i+=1
+    j+=1    
+
+print(freq)
